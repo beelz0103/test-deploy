ea916704694924c80137318a0b02ae71dc931c3d
Tet <anabathula.praveen103@gmail.com>
2023-03-21 09:18:44 +0530

    AAAAAAAAAAAAAAA

--- app.js
+++ app.js
@@ -15,10 +15,7 @@ mongoose.set("strictQuery", false);
 
 const dev_db_url = `mongodb+srv://tet:vGfeDKnwCNN8a7XS@cluster0.djbois6.mongodb.net/mini-message-board`;
 
-const mongoDB = dev_db_url;
-// const mongoDB = process.env.MONGO_URL || dev_db_url;
-// const mongoDB =
-//   "mongodb://mongo:gRaMkcvmFv2R0FU4KR3d@containers-us-west-36.railway.app:6718";
+const mongoDB = process.env.MONGO_URL || dev_db_url;
 
 mongodb: main().catch((err) => console.log(err));
 async function main() {
